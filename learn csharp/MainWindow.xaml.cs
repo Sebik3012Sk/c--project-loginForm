7979564faff729f98a565068717cf510a79c3bdb
Sebik3012Sk <sebik-kucera@hotmail.com>
2022-10-23 13:50:40 +0200

    login form

---
+++ learn csharp/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace learn_csharp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Login_past_click(object sender,RoutedEventArgs e)
+        {
+            string data_username = input_username.Text.ToString();
+            string data_password = input_password.Password.ToString();
+
+;           string[] list_password = { "7259" };
+
+            if(data_username == "Sebastian Kučera" && data_password == list_password[0])
+            {
+                validData.Text = "Your data is valid :D";
+                validData.Foreground = new SolidColorBrush(Colors.DarkBlue);
+                validData.FontSize = 18;
+            }
+
+        }
+    }
+}
